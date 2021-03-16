30bbc6b1b197f76710633419b82e411f5d4f14aa
KusakaIzumi <izumiyorokobi@gmail.com>
2021-03-16 21:30:01 +0900

    コメントクリック削除、ipアドレスver2

--- js/comment.js
+++ js/comment.js
@@ -42,7 +42,7 @@ $(function(){
 
     var timer = msg.count * 3000;
 
-    console.log(msg.ipadress);
+    console.log("comment:" + msg.msg + ",  ip:" + msg.ipadress);
 
     if( document.getElementById(msg.syurui) != null ){
       if(document.getElementById(msg.syurui).style.display == "block" && msg.count > 0){
